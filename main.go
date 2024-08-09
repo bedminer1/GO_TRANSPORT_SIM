d75893f2a4d40b1437f4b0096f23c587a492d513
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-08-09 15:29:13 +0800

    defined structs

--- main.go
+++ main.go
@@ -1,6 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type Passenger struct {
+	source int
+	destination int
+	arrivalTime time.Time
+}
+
+type Bus struct {
+	id int
+	route []int
+	capacity int
+	passengers []Passenger
+	location int
+}
+
+type BusStop struct {
+	id int
+	queue []Passenger
+}
+
+type System struct {
+	busStops []BusStop
+	buses []Bus
+	terminal int
+}
 
 func main() {
 	fmt.Println("Hello World")
